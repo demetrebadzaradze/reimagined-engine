85ead2fdc43a179c98e43f69a959a7ee77794e09
TG_3W3p <128867020+demetrebadzaradze@users.noreply.github.com>
2025-04-07 22:15:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const { createFFmpeg, fetchFile } = FFmpeg;
-const ffmpeg = createFFmpeg({ log: true });
+const ffmpeg = createFFmpeg({
+  log: true,
+  corePath: 'https://unpkg.com/@ffmpeg/ffmpeg@0.10.0/dist/ffmpeg.min.js',
+  mainName: 'main',
+  worker: false,
+});
+
 
 document.getElementById('convertBtn').onclick = async () => {
   const file = document.getElementById('uploader').files[0];
@@ -14,6 +20,7 @@ document.getElementById('convertBtn').onclick = async () => {
   await ffmpeg.run('-i', 'input.avi', 'output.mp4');
 
   const data = ffmpeg.FS('readFile', 'output.mp4');
+               
   const url = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
 
   const a = document.createElement('a');
@@ -24,3 +31,16 @@ document.getElementById('convertBtn').onclick = async () => {
 
   document.getElementById('status').textContent = 'Done!';
 };
+
+// const fs = require('fs');
+// const { createFFmpeg, fetchFile } = require('@ffmpeg/ffmpeg');
+
+// const ffmpeg = createFFmpeg({ log: true });
+
+// (async () => {
+//   await ffmpeg.load();
+//   ffmpeg.FS('writeFile', 'test.avi', await fetchFile('./test.avi'));
+//   await ffmpeg.run('-i', 'test.avi', 'test.mp4');
+//   await fs.promises.writeFile('./test.mp4', ffmpeg.FS('readFile', 'test.mp4'));
+//   process.exit(0);
+// })();
